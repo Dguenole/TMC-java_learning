93feccbaa903755ff39fc7053a22b6be700501dd
guenole das <guenoledas@proton.me>
2026-07-23 17:25:25 +0000

    part04 01 à 17

---
+++ part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,12 @@
+public class Whistle {
+
+    private String sound;
+
+    public Whistle(String whileSound) {
+        this.sound = whileSound;
+    }
+
+    public void sound() {
+        System.out.println(this.sound);
+    }
+}
